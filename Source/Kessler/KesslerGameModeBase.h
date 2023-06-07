a97b03b76627b9db7bfaf708f42b8f03a4958f24
JbondWhitethornGames <james.m.bond1@gmail.com>
2023-06-07 19:02:06 -0400

    Actual Initial Commit

---
+++ Source/Kessler/KesslerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KesslerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KESSLER_API AKesslerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
